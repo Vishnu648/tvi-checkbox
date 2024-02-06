0c6d42e3c9e4b1477e859861791edd91035511f2
Vishnu V S <vishnusenthil222@gmail.com>
2024-02-06 17:35:52 +0530

    listing everything

---
+++ script.js
@@ -0,0 +1,358 @@
+const array = [
+  {
+    evtuid: 345,
+    regtype_description: "EDUCATION & EXHIBITS - BASIC",
+    regtype: "001",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EDUCATION & EXHIBITS - PREMIUM",
+    regtype: "002",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EDUCATION & EXHIBITS - ELITE",
+    regtype: "003",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EDUCATION & EXHIBITS - NON MBR",
+    regtype: "004",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITS HALL ONLY - BASIC",
+    regtype: "005",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITS HALL ONLY - PREMIUM",
+    regtype: "006",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITS HALL ONLY - ELITE",
+    regtype: "007",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITS HALL ONLY - NON MBR",
+    regtype: "008",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "VIP - BASIC MBR",
+    regtype: "009",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "VIP - PREMIUM MBR",
+    regtype: "010",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "VIP - ELITE MBR",
+    regtype: "011",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "VIP - NON MBR",
+    regtype: "012",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "SPEAKER",
+    regtype: "014",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - APPROVED NON-MEMBER",
+    regtype: "015",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "Press - Pending NON-MEMBER",
+    regtype: "016",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - DENIED",
+    regtype: "017",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR KEY CONTACT",
+    regtype: "018",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DEALER - NON MBR",
+    regtype: "019",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DISTRIBUTOR - NONMBR",
+    regtype: "020",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR INDEPENDENT REP NON",
+    regtype: "021",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR MANUFACTURER NONMBR",
+    regtype: "022",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR PRODUCER NON MBR",
+    regtype: "023",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR OTHER - NON MBR",
+    regtype: "024",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CONTRACTOR",
+    regtype: "025",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DEALER - BASIC",
+    regtype: "029",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DEALER - PREMIUM",
+    regtype: "030",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DEALER - ELITE",
+    regtype: "031",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DISTRIBUTOR - BASIC",
+    regtype: "032",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DISTRIBUTOR - PREMIM",
+    regtype: "033",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR DISTRIBUTOR - ELITE",
+    regtype: "034",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR INDEPENDENT REP - BC",
+    regtype: "035",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR INDEPENDENT REP - PM",
+    regtype: "036",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR INDEPENDENT REP - EE",
+    regtype: "037",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR MANUFACTURER - BASIC",
+    regtype: "038",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR MANUFACTURER - PREMM",
+    regtype: "039",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR MANUFACTURER - ELITE",
+    regtype: "040",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR PRODUCER - BASIC",
+    regtype: "041",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR PRODUCER - PREMIUM",
+    regtype: "042",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR PRODUCER - ELITE",
+    regtype: "043",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR OTHER - BASIC",
+    regtype: "044",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR OTHER - PREMIUM",
+    regtype: "045",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "EXHIBITOR OTHER - ELITE",
+    regtype: "046",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - APPROVED BASIC",
+    regtype: "047",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - APPROVED PREMIUM",
+    regtype: "048",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - APPROVED ELITE",
+    regtype: "049",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - PENDING BASIC",
+    regtype: "050",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - PENDING PREMIUM",
+    regtype: "051",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "PRESS - PENDING ELITE",
+    regtype: "052",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "SPEAKER - BASIC",
+    regtype: "053",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "SPEAKER - PREMIUM",
+    regtype: "054",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "SPEAKER - ELITE",
+    regtype: "055",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "STAFF - NONMEMBER",
+    regtype: "056",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "STAFF - BASIC",
+    regtype: "057",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "STAFF - PREMIUM",
+    regtype: "058",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "STAFF - ELITE",
+    regtype: "059",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "BLOCKED REGISTRATION",
+    regtype: "BLC",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED ATTENDEE DUPLICATE",
+    regtype: "CD",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED EXHIBITOR DUPLICATE",
+    regtype: "CE",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED ATTENDEE",
+    regtype: "CN",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED PRESS",
+    regtype: "CP",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED SPEAKER",
+    regtype: "CS",
+  },
+  {
+    evtuid: 345,
+    regtype_description: "CANCELLED EXHIBITOR",
+    regtype: "CX",
+  },
+];
+
+let elements = document.getElementById("checkbox");
+let headings = [];
+let printedHeadings = [];
+let headingId = 0;
+
+array.map((c, i) => {
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("id", headingId);
+
+  let title = document.createElement("label");
+  title.setAttribute("for", headingId);
+  headingId++;
+  let subHeading = c.regtype_description.split(" ");
+  title.textContent = subHeading[0];
+  {
+    !headings.includes(subHeading[0]) ? headings.push(subHeading[0]) : "";
+  }
+  //   title.style.display = "inline";
+  title.style.fontWeight = "bold";
+  title.style.fontSize = "20px";
+
+  let newLine = document.createElement("br");
+
+  let regtype = document.createElement("input");
+  regtype.setAttribute("id", c.regtype);
+  regtype.setAttribute("type", "checkbox");
+
+  let regtypeLabel = document.createElement("label");
+  regtypeLabel.setAttribute("for", c.regtype);
+  regtypeLabel.textContent = subHeading;
+
+  //   !printedHeadings.includes(subHeading[0]) ? elements.appendChild(title)  : null;
+  if (!printedHeadings.includes(subHeading[0])) {
+    elements.appendChild(checkbox);
+    elements.appendChild(title);
+    elements.appendChild(newLine);
+  } else {
+  }
+  printedHeadings.push(subHeading[0]);
+  elements.appendChild(newLine);
+  elements.appendChild(regtype);
+  elements.appendChild(regtypeLabel);
+  elements.appendChild(newLine);
+});
